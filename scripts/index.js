cf936b5db2a5f63d2ed3db908e9ffa78cd0038c1
Miguel Bravo <mig.om@hotmail.fr>
2021-04-29 12:47:11 +0200

    Formulaire fini

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+infos = document.getElementById("inscription");
+
+// affichage d'une alerte de bienvenue
+var afficherInfos = (form) => {
+    alert("Bonjour " + form.prenom.value + " "+ form.nom.value + ", votre inscription s'est bien déroulée ");
+}
